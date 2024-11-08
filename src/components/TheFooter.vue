7b0a66245b1d258c6b35763b44b9cf4e04c4f185
Joel R <129562199+joelrennert@users.noreply.github.com>
2024-11-07 22:26:29 -0500

    deleted files

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -5,55 +5,62 @@
       <a href="https://github.com/joelrennert" target="_blank">github</a>
       》<a href="https://www.linkedin.com/in/joelrennert/" target="_blank">linkedin</a>
     </div>
-    <div class="container2">🍄</div>
+    <div class="container2">
+      <span class="emoji">🍄</span>
+    </div>
   </footer>
 </template>
+
 <script>
 export default {}
 </script>
+
 <style scoped>
+.emoji {
+  display: inline-block;
+}
+
+.emoji:hover {
+  animation: centeredBouncy 0.5s ease;
+}
+
 @keyframes centeredBouncy {
   0% {
-    transform: scale(1); 
+    transform: scale(1);
   }
   30% {
     transform: scale(1.1);
   }
   50% {
-    transform: scale(0.95);
+    transform: scale(1.95);
   }
   70% {
     transform: scale(1.05);
   }
   100% {
     transform: scale(1);
-  }
+}
+}
+
+.container1 {
+  display: flex;
+  align-items: center;
+  gap: 10px;
 }
 
 .container2 {
   display: flex;
-  flex-grow: 1;
-  justify-content: right;
-  padding-right: 20px;
-}
-
-a {
-  color: black;
-}
-
-a:hover {
-  background-color: white;
-  font-size: 0.8rem;
-  padding: 3px;
+  align-items: center;
 }
 
 footer {
   font-family: 'IBM Plex Mono';
   z-index: 1;
   display: flex;
+  justify-content: space-between;
   align-items: center;
   width: 100vw;
-  height: 20px;
+  height: 40px;
   bottom: 0;
   left: 0;
   background-color: white;
@@ -65,5 +72,15 @@ footer {
   border-top-right-radius: 15px;
   padding: 12px;
   font-size: 0.8rem;
+}
+
+a {
+  color: black;
+}
+
+a:hover {
+  background-color: white;
+  font-size: 0.8rem;
+  padding: 3px;
 }
 </style>
